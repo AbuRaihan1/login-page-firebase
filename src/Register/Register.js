51efd07e1706df43a61ba79739fc1f4be740a675
Abu Raihan <raihanahmed01973@gmail.com>
2023-01-20 21:13:11 +0600

    done mobile layout

---
+++ src/Register/Register.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Register.css'
+const Register = () => {
+    return (
+        <div className='mobile-container'>
+          <div className="cam-area"></div>
+          <div className="bottom-circle"></div>
+        </div>
+    );
+};
+
+export default Register;
