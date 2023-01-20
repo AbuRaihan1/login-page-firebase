3ceb08adc1ac8349e0b47bbbfff23babf1b097b6
Abu Raihan <raihanahmed01973@gmail.com>
2023-01-20 23:40:30 +0600

    done registration page

--- src/Register/Register.js
+++ src/Register/Register.js
@@ -1,12 +1,48 @@
-import React from 'react';
-import './Register.css'
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Register.css";
 const Register = () => {
-    return (
-        <div className='mobile-container'>
-          <div className="cam-area"></div>
-          <div className="bottom-circle"></div>
+  return (
+    <div className="mobile-container">
+      <div className="cam-area"></div>
+      <div className="bottom-circle"></div>
+
+      <div className="signup-wrapper">
+        <h2>Sign up</h2>
+        <div className="signin-third-party">
+          <button className="btn"> github</button>
+          <button className="btn"> Facebook</button>
         </div>
-    );
+
+        <div className="or">or</div>
+        <div className="info-area">
+          <label htmlFor="" className="name-area">
+            <span>
+              {" "}
+              First Name <input type="text" required />
+            </span>
+            <span>
+              Last Name <input type="text" />
+            </span>
+          </label>
+
+          <label htmlFor="">
+            Email
+            <input type="email" required />
+          </label>
+
+          <label htmlFor="">
+            Password
+            <input type="password" required />
+          </label>
+          <button className="btn crate-account-btn">Create Account</button>
+          <small>
+            Already have an account?  <a href="#">Login here</a>
+          </small>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Register;
